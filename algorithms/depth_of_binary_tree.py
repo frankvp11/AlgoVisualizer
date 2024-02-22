a45f62777ca1ccdfcf09bcfae6a84423d8770564
frankvp11 <frankvanpaassen3@gmail.com>
2024-02-22 15:44:54 -0500

    made it embedded into the tree - need to fix overlaps

--- algorithms/depth_of_binary_tree.py
+++ algorithms/depth_of_binary_tree.py
@@ -7,23 +7,24 @@ from ModelMaker.graphicsSVG2 import Rectangle, Text, ShapeCollection, Circle, Po
 
 
 class BinaryTree:
-    def __init__(self, value) -> None:
+    def __init__(self, value, x, y) -> None:
         self.value = value
         self.left = None
         self.right = None
+        self.polygon = [Circle.Circle(x, y, 10), Text.Text(str(value), x, y-20, "black", 10)]
 
-    def insert(self, value):
+    def insert(self, value, x, y):
         if value < self.value:
             if self.left is None:
-                self.left = BinaryTree(value)
+                self.left = BinaryTree(value, x-25, y+25)
             else:
-                self.left.insert(value)
+                self.left.insert(value, x-25, y+25)
         else:
             if self.right is None:
-                self.right = BinaryTree(value)
+                self.right = BinaryTree(value, x+25, y+25)
             else:
-                self.right.insert(value)
-
+                self.right.insert(value, x+25, y+25)
+    
 
 class SVGContent():
     def __init__(self, polygons) -> None:
@@ -35,28 +36,22 @@ class SVGContent():
         self.content = self.polygons.to_svg()
 
 
-def make_tree_polygons(tree, x, y):
+def make_tree_polygons(tree):
     if tree is None:
         return []
     else:
-        polys = []
-
-        polys.append(Text.Text(str(tree.value), x, y-20, "black", 10))
-        polys.append(Circle.Circle(x, y, 10))
-        left = make_tree_polygons(tree.left, x - 25, y + 25)
-        right = make_tree_polygons(tree.right, x + 25, y + 25)
-        return polys + left + right
+        return tree.polygon + make_tree_polygons(tree.left) + make_tree_polygons(tree.right)
         
 
 def make_tree():
     # num_nodes = random.randint(5, 10)
     num_nodes = 5
     values = random.sample(range(1, 100), num_nodes)
-    tree = BinaryTree(values[0])
+    tree = BinaryTree(values[0], 100, 50)
     for value in values[1:]:
-        tree.insert(value)
+        tree.insert(value, 100, 50)
 
-    polygons = make_tree_polygons(tree, 100, 50)    
+    polygons = make_tree_polygons(tree)    
 
     group = ShapeCollection.ShapeCollection(polygons)
     group.move_all(0, 0)
