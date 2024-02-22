8756f265ed80a5ec5b08947f687ce1a3d87f8114
frankvp11 <frankvanpaassen3@gmail.com>
2024-02-22 13:58:12 -0500

    routing

--- cards.py
+++ cards.py
@@ -1,6 +1,7 @@
 from nicegui import ui, app, APIRouter
 import algorithms.depth_of_binary_tree
-
+import algorithms.dijkstras
+import algorithms.binary_search
 
 router = APIRouter(prefix='/algorithms')
 
@@ -12,11 +13,7 @@ app.include_router(router)
 
 
 
-def show_page(event, page):
 
-    print(page)
-    
-    # print(event)
 
 def add():
     pages = ["Depth of Binary Tree", "Dijkstras", "Binary Search"]
@@ -26,7 +23,7 @@ def add():
         with ui.link(
                 text="",
                 target=f"/algorithms/{targets[i]}").style("text-decoration:none; color: black; display: flex; float: left;"):           
-              with ui.card().on("click", lambda e, i=i: show_page(e, pages[i])):
+              with ui.card():
                 ui.label(pages[i])
                 ui.image(icons[i]).style("width: 100px; height: 100px;")
 
@@ -35,3 +32,13 @@ def add():
 @ui.page('/algorithms/depth_of_binary_tree')
 def main():
     algorithms.depth_of_binary_tree.add()
+
+@ui.page('/algorithms/dijkstras')
+def main():
+    algorithms.dijkstras.add()
+
+@ui.page('/algorithms/binary_search')
+def main():
+    algorithms.binary_search.add()
+
+
