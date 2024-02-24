abc9929364b41a565cb22e5683b497571d8dcff6
frankvp11 <frankvanpaassen3@gmail.com>
2024-02-24 14:36:16 -0500

    rendered graph - current bug : svg's are duplicating?

--- algorithms/depth_first_search.py
+++ algorithms/depth_first_search.py
@@ -1,6 +1,7 @@
 
 import sys
 import math
+from math import sqrt
 import random
 import networkx as nx
 
@@ -11,6 +12,7 @@ from ModelMaker.graphicsSVG2.Arrow import Arrow
 from ModelMaker.graphicsSVG2.ShapeCollection import ShapeCollection
 from ModelMaker.graphicsSVG2.Text import Text
 from ModelMaker.graphicsSVG2.Line import Line
+from ModelMaker.graphicsSVG2.Rectangle import Rectangle
 from nicegui import ui, app
 
 
@@ -20,42 +22,70 @@ class Node:
         self.data = data
         self.x = kwargs.get('x', 0)
         self.y = kwargs.get('y', 0)
-        self.polygon = ShapeCollection()
         self.id = kwargs.get('id', 0)
+        self.neighbors = []
+        self.circle = None
+        self.text = None
 
     def create_polygon(self):
-        circle = Circle(self.x, self.y, 15, color='darkgray', transparency=0.9)
-        circle.give_outline("black", thickness=1)
-        text = Text(self.data, self.x, self.y, fontsize=10)
-        self.polygon.add_polygon(circle)
-        self.polygon.add_polygon(text)
-        return self.polygon
-
+        self.circle = Circle(self.x, self.y, 15, color='darkgray', transparency=0.9)
+        self.circle.give_outline("black", thickness=1)
+        self.text = Text(self.data, self.x, self.y, fontsize=10)
+        return ShapeCollection([self.circle, self.text])
 
 
 
 class Graph:
-    def __init__(self) -> None:
+    def __init__(self, **kwargs) -> None:
         self.nodes = []
         self.edges = []
         self.node_polygons = []
         self.edge_polygons = []
-        self.polygons = []
+        self.polygons = ShapeCollection()
+        self.adjacency_list = kwargs.get('adjacency_list', {})
+        self.content = ""
+        self.stack = []
+        self.visited = [False] * 1000
+        self.previous = None
+        self.done = False
+        self.timer = None
+        self.stack_content = ""
+    
+
+    def render_stack(self):
+        shapes = ShapeCollection()
+        for node in self.stack:
+            rect = Rectangle(0, 0, 100, 50, color='lightgray', transparency=0.9)
+            rect.give_outline("black", thickness=1)
+            text = Text(node.data, 30, 40, fontsize=20)
+            shapes.add_polygon(rect)
+            shapes.add_polygon(text)
+        
+        self.stack_content = shapes.to_svg()
 
     def add_node(self, data, x, y, id):
         node = Node(data, x=x, y=y, id=id)
         self.nodes.append(node)
         polygon = node.create_polygon()
         self.node_polygons.append(polygon.polygons[0])
-        self.node_polygons.append(polygon.polygons[1])  # Assuming polygons[0] represents the circle
+        self.node_polygons.append(polygon.polygons[1])  
         return node
 
     def add_edge(self, start, end, start_id, end_id):
         self.edges.append((start, end))
         start_node = self.find_node(start, start_id)
         end_node = self.find_node(end, end_id)
-        line = Line(start_node.x, start_node.y, end_node.x, end_node.y, color='black')
+        dx = end_node.x - start_node.x
+        dy = end_node.y - start_node.y
+        distance = sqrt(dx ** 2 + dy ** 2)        
+        start_intersection_x = start_node.x + (dx / distance) * 15
+        start_intersection_y = start_node.y + (dy / distance) * 15        
+        end_intersection_x = end_node.x - (dx / distance) * 15
+        end_intersection_y = end_node.y - (dy / distance) * 15        
+        line = Line(start_intersection_x, start_intersection_y, end_intersection_x, end_intersection_y, color='black')
         self.edge_polygons.append(line)
+        start_node.neighbors.append(end_node)
+        end_node.neighbors.append(start_node)
     
     def find_node(self, data, id):
         for node in self.nodes:
@@ -63,13 +93,54 @@ class Graph:
                 return node
         return None
 
-    def to_svg(self):
-        collection = ShapeCollection()
+    def make_svg(self):
+        
         for polygon in self.edge_polygons:
-            collection.add_polygon(polygon)
+            self.polygons.add_polygon(polygon)
         for polygon in self.node_polygons:
-            collection.add_polygon(polygon)
-        return collection.to_svg()
+            self.polygons.add_polygon(polygon)
+        
+        self.content = self.polygons.to_svg()
+
+    def dfs_one_call(self):
+        if self.stack:
+            current = self.stack.pop()
+            # current.circle.color = "red"
+            self.polygons.polygons[self.polygons.polygons.index(current.circle)].color = "red"
+            if self.previous:
+                self.polygons.polygons[self.polygons.polygons.index(self.previous.circle)].color = "lightgray"
+            self.make_svg()
+            self.previous  = current
+            for neighbor in current.neighbors:
+                if not self.visited[neighbor.data]:
+                    self.visited[neighbor.data] = True
+                    self.stack.append(neighbor)
+            if self.stack == []:
+                self.done = True
+                
+        else:
+            self.make_svg()
+            self.previous = None
+            self.stack = []
+            self.visited = [False] * 1000
+            return
+        
+    def dfs_animated(self, start=None):
+        if self.done: 
+            self.polygons.polygons[self.polygons.polygons.index(self.previous.circle)].color = "lightgray"
+            self.make_svg()
+            self.timer.cancel()
+            return
+        if self.stack == [] :
+            self.stack = [start]
+            self.visited[start.data] = True
+            return
+        self.dfs_one_call()
+        self.render_stack()
+
+    def start_timer(self):
+        self.timer = ui.timer(3, self.dfs_animated)
+
 
 def add():
 
@@ -82,26 +153,40 @@ def add():
     graph = nx.MultiGraph()
     graph2 = Graph()
     adjaceny_list = {
-        1: [2, 3, 5],
+        1: [2, 3],
         2: [3],
-        3: [5],
+        3: [],
+        4: [5],
         5: [6],
         6: [],
+        7: [8],
+        8: [9],
+        9: [7, 2, 4]
+
+        
     }
     for key in adjaceny_list:
         graph.add_node(key)
         for val in adjaceny_list[key]:
             graph.add_edge(key, val)
     pos = nx.kamada_kawai_layout(graph)
-    graph2.nodes = [graph2.add_node(key, 150+ 100*pos[key][0], 150+100*pos[key][1], id(key)) for key in adjaceny_list]
+    graph2.nodes = [graph2.add_node(key, 250+ 200*pos[key][0], 250+200*pos[key][1], id(key)) for key in adjaceny_list]
     
     for key in adjaceny_list:
         for val in adjaceny_list[key]:
             graph2.add_edge(key, val, id(key), id(val))
+            
+    # print(len(graph2.nodes[0].polygon.polygons))
+    # print(graph2.nodes[0].polygon.polygons)
+    with ui.row():
+        with ui.column().style("width: 50vw; "):
+            graph2.dfs_animated(graph2.nodes[0])
+            ui.button("Depth First Search", on_click=lambda e : graph2.start_timer())
+            graph2.make_svg()
+            image = ui.interactive_image("/static/dfssvg.svg").style("width: 100vw;")
+            image.bind_content_from(graph2, 'content')
 
-    for node in graph2.nodes:
-        print(node.data, node.x, node.y, node.id)
-
-
-    image = ui.interactive_image("/static/dfssvg.svg")
-    image.content = graph2.to_svg()
+        with ui.column().style("width:40vw;"):
+            ui.label("Stack:")
+            image2 = ui.interactive_image("/static/dfssvg_stack.svg").style("width: 100vw;")
+            image2.bind_content_from(graph2, 'stack_content')
